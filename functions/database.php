9c4b8fdf06154ed3b1325dd78c5fae54699affa6
Elrapiz <rafael.pizarro.elrapiz@gmail.com>
2024-05-06 20:21:36 +0800

    Initial commit

---
+++ functions/database.php
@@ -0,0 +1,20 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'contacts');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+try {
+    $dbc = new PDO(
+        'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . '',
+        DB_USER,
+        DB_PASSWORD,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]);
+
+} catch (PDOException $e) {
+    die($e->getMessage() . '<br/>' . $e->getCode());
+}
